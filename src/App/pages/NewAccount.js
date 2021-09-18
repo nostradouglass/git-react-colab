555db9d6b38f65d0c149ee9bf3a79b8248561d64
Kelly Douglass <kdouglass@gmail.com>
2021-09-18 11:40:14 -0500

    create user basics working, need to fix cors error

--- src/App/pages/NewAccount.js
+++ src/App/pages/NewAccount.js
@@ -1,9 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+
 
 
 const NewAccount = () => {
+
+    const [first, setFirst] = useState('')
+    const [last, setLast] = useState('')
+    const [email, setEmail] = useState('')
+    const [pass, setPass] = useState('')
+
+    const createUser = () => {
+        axios.post('localhost:3000/users/', {
+            firstName: 'Fred',
+            lastName: 'Flintstone',
+            email: 'FFlintstone@stonemail.com',
+            password: "rubblerubble"
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    const createUserSubmit = (e) => {
+        e.preventDefault()
+        console.log("submit")
+        createUser()
+    }
+
     return (
-        <h3>Create new User Account</h3>
+        <div>
+            <form  onSubmit={createUserSubmit} >
+                <h2 className="text-center">
+                    Create new User
+                </h2>
+
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input type="email" 
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="form-control"
+                        placeholder="geeks@example.com" />
+                </div>
+                <div className="form-group">
+                    <label>First</label>
+                    <input type="text" 
+                    value={first}
+                    onChange={(e) => setFirst(e.target.value)}
+                    className="form-control"
+                        placeholder="first" />
+                </div>
+                <div className="form-group">
+                    <label>Last</label>
+                    <input type="text" 
+                    value={last}
+                    onChange={(e) => setLast(e.target.value)}
+                    className="form-control"
+                        placeholder="Last" />
+                </div>
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password"
+                    value={pass}
+                    onChange={(e) => setPass(e.target.value)}
+                    className="form-control"
+                        placeholder="Pass" />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+
+        </div>
     )
 }
 
