ebfa13066ef116f0a06555e0cc1037b01020e088
Pedro <pedrorellana21@gmail.com>
2021-10-02 11:18:09 -0500

    change text on delete button

--- src/App/pages/DeleteUser.js
+++ src/App/pages/DeleteUser.js
@@ -25,7 +25,7 @@ const DeleteUser = () => {
             <div className ="input-group mb-3">
            
                 <input id = "userIdInput" type="text" className= "form-control" value = {userID} onChange = {(e) => setUserID(e.target.value)}/>
-                <button className ="btn btn-outline-secondary" type="button"  onClick = {onDeleteButtonClick}>Delete User</button>
+                <button className ="btn btn-outline-secondary" type="button"  onClick = {onDeleteButtonClick}>Delete this User</button>
             </div>
            
         </div>
