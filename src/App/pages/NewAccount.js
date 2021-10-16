8a1ef2cef9401367ded91ac4c0c30747a8d72f94
Kelly Douglass <kdouglass@gmail.com>
2021-10-16 11:13:08 -0500

    format css and fix clear user data on creation

--- src/App/pages/NewAccount.js
+++ src/App/pages/NewAccount.js
@@ -8,6 +8,7 @@ const NewAccount = () => {
     const [last, setLast] = useState('')
     const [email, setEmail] = useState('')
     const [pass, setPass] = useState('')
+    const [showForm, setShowForm] = useState(true)
 
     const createUser = () => {
         axios.post('http://localhost:3000/users', {
@@ -18,6 +19,11 @@ const NewAccount = () => {
         })
             .then(function (response) {
                 console.log(response);
+                setFirst('')
+                setLast('')
+                setEmail('')
+                setPass('')
+                setShowForm(false)
             })
             .catch(function (error) {
                 console.log(error);
@@ -29,50 +35,81 @@ const NewAccount = () => {
         createUser()
     }
 
-    return (
-        <div>
-            <form  onSubmit={createUserSubmit} >
-                <h2 className="text-center">
-                    Create new User
-                </h2>
+    const showUserForm = () => {
+        if (showForm) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
 
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" 
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    className="form-control"
-                        placeholder="geeks@example.com" />
-                </div>
-                <div className="form-group">
-                    <label>First</label>
-                    <input type="text" 
-                    value={first}
-                    onChange={(e) => setFirst(e.target.value)}
-                    className="form-control"
-                        placeholder="first" />
-                </div>
-                <div className="form-group">
-                    <label>Last</label>
-                    <input type="text" 
-                    value={last}
-                    onChange={(e) => setLast(e.target.value)}
-                    className="form-control"
-                        placeholder="Last" />
-                </div>
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password"
-                    value={pass}
-                    onChange={(e) => setPass(e.target.value)}
-                    className="form-control"
-                        placeholder="Pass" />
-                </div>
-                <button type="submit">Submit</button>
-            </form>
+                        </div><div className="col">
 
-        </div>
-    )
+                            <form onSubmit={createUserSubmit} >
+                                <h2 style={{margin: "50px"}} lassName="text-center">
+                                    Create new User
+                                </h2>
+
+                                <div className="form-group">
+                                    <label>Email address</label>
+                                    <input type="email"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        className="form-control"
+                                        placeholder="geeks@example.com" />
+                                </div>
+                                <div className="form-group">
+                                    <label>First</label>
+                                    <input type="text"
+                                        value={first}
+                                        onChange={(e) => setFirst(e.target.value)}
+                                        className="form-control"
+                                        placeholder="first" />
+                                </div>
+                                <div className="form-group">
+                                    <label>Last</label>
+                                    <input type="text"
+                                        value={last}
+                                        onChange={(e) => setLast(e.target.value)}
+                                        className="form-control"
+                                        placeholder="Last" />
+                                </div>
+                                <div className="form-group">
+                                    <label>Password</label>
+                                    <input type="password"
+                                        value={pass}
+                                        onChange={(e) => setPass(e.target.value)}
+                                        className="form-control"
+                                        placeholder="Pass" />
+                                </div>
+                                <button style={{width: "100%"}} className="btn btn-primary" type="submit">Submit</button>
+                            </form>
+                        </div>
+                        <div className="col">
+
+                        </div>
+                    </div>
+                </div>
+            )
+        } else {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <div className="col"></div>
+                        <div style={{marginTop:"100px"}} className="col">
+                            <h4 className="text-center">User created.</h4>
+                            <button onClick={() => setShowForm(true)} style={{width: "100%"}} className="btn btn-primary">Create another user</button>
+                        </div>
+                        <div className="col"></div>
+                    </div>
+
+                </div>
+            )
+        }
+    }
+
+    return showUserForm()
+
+
 }
 
 export default NewAccount
