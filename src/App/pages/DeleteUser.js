4a018f2e79283ee46fa127ecd263f1a10cdc29f1
Pedro <pedrorellana21@gmail.com>
2021-11-13 09:28:03 -0600

    commit

--- src/App/pages/DeleteUser.js
+++ src/App/pages/DeleteUser.js
@@ -1,33 +1,42 @@
 
 
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
 import axios from 'axios';
 
 
 const DeleteUser = () => {
 
     const [userID, setUserID] = useState('');
+    const inputRef = useRef();
 
-    const onDeleteButtonClick = () => {
+    const onDeleteButtonClick = (e) => {
+        e.preventDefault();
+        const inputID = inputRef.current
         if(userID){
             axios.delete(`http://localhost:3000/users/${userID}`).then((res) => {
-                console.log(res)
+               
             })
+            setUserID('');
         }
-     
-     
     }
 
     return (
 
-        <div>
+        <div className = "container">
+            <br/>
+            <form onSubmit = {onDeleteButtonClick}>
+                
+            <div className = "col-12">
+                
             <label htmlFor = "userIdInput" >Delete a User by ID </label> 
             <div className ="input-group mb-3">
            
-                <input id = "userIdInput" type="text" className= "form-control" value = {userID} onChange = {(e) => setUserID(e.target.value)}/>
-                <button className ="btn btn-outline-secondary" type="button"  onClick = {onDeleteButtonClick}>Delete this User</button>
+                <input id = "userIdInput" ref = {inputRef} type="text" className= "form-control" value = {userID} onChange = {(e) => setUserID(e.target.value)}/>
+                <button className ="btn btn-outline-secondary" type="submit">Delete a User</button>
             </div>
-           
+            </div>
+
+            </form>
         </div>
        
 
