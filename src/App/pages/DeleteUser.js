2afff37dd8b47a1838784da0075bc8aa2dd884fd
Pedro <pedrorellana21@gmail.com>
2021-11-13 10:32:28 -0600

    change UI

--- src/App/pages/DeleteUser.js
+++ src/App/pages/DeleteUser.js
@@ -1,44 +1,76 @@
 
 
-import React, {useState, useRef} from 'react'
+import React, { useState } from 'react'
 import axios from 'axios';
+
 
 
 const DeleteUser = () => {
 
     const [userID, setUserID] = useState('');
-    const inputRef = useRef();
 
-    const onDeleteButtonClick = (e) => {
-        e.preventDefault();
-        const inputID = inputRef.current
-        if(userID){
+    const onDeleteUserSubmit = (e) => {
+        e.preventDefault()
+        if (userID) {
             axios.delete(`http://localhost:3000/users/${userID}`).then((res) => {
-               
+                console.log(res)
+                setUserID = ''
             })
-            setUserID('');
         }
     }
 
     return (
 
-        <div className = "container">
-            <br/>
-            <form onSubmit = {onDeleteButtonClick}>
-                
-            <div className = "col-12">
-                
-            <label htmlFor = "userIdInput" >Delete a User by ID </label> 
+        <div>
+            {/* <label htmlFor = "userIdInput" >Delete a User by ID </label> 
             <div className ="input-group mb-3">
            
-                <input id = "userIdInput" ref = {inputRef} type="text" className= "form-control" value = {userID} onChange = {(e) => setUserID(e.target.value)}/>
-                <button className ="btn btn-outline-secondary" type="submit">Delete a User</button>
-            </div>
+                <input id = "userIdInput" type="text" className= "form-control" value = {userID} onChange = {(e) => setUserID(e.target.value)}/>
+                <button className ="btn btn-outline-secondary" type="button"  onClick = {onDeleteButtonClick}>Delete this User</button>
+            </div> */}
+
+            <div className="container container-su">
+               
+                <div className="row">
+
+                    <div className="col"></div>
+
+                    <div className="col">
+                        <h2 style={{ margin: "50px" }} className="text-center">
+                            Delete User
+                        </h2>
+                        <form onSubmit={onDeleteUserSubmit}>
+
+                            <div className="form-group">
+                                <label>User ID</label>
+                                <input type="text"
+                                    value={userID}
+                                    onChange={(e) => setUserID(e.target.value)}
+                                    className="form-control" />
+                            </div>
+
+                            <div>
+                                <button className="btn btn-primary"
+                                    type="submit"
+                                    style={{ width: "100%" }}>
+                                    Delete
+                                </button>
+                            </div>
+
+                        </form>
+
+                    </div>
+
+                    <div className="col"></div>
+
+                </div>
             </div>
 
-            </form>
+
+
+
         </div>
-       
+
 
     )
 
